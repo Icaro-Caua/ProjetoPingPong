8f97a9eb2154bfbd47018481a31f1dd1e279a4da
ErickFurtado0 <106315916+ErickFurtado0@users.noreply.github.com>
2022-06-13 15:35:50 -0300

    UpdateProject

---
+++ CampProject/CampProject/Controllers/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Web.Mvc;
+
+namespace CampProject.Controllers
+{
+    public class PlayerController : Controller
+    {
+        // GET: Player
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Player/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // SET: Player/Create
+        public string SetName(string playerName)
+        {
+            if (playerName == string.Empty) {
+                return ExceptionContext(ModelError);
+            }
+            else (playerName != string.Empty){
+                return SetName();
+            }
+        }
+
+        // POST: Player/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Player/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Player/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Player/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Player/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
