7ff98dd2c41315011be5fe2dbb7fae9e4ffdfc82
ErickFurtado0 <106315916+ErickFurtado0@users.noreply.github.com>
2022-06-14 15:02:43 -0300

    Added Project Version 2.0

---
+++ IATechamp1.0/IATechamp1.0/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace IATechamp1._0
+{
+    public class Player
+    {
+        public int IdPlayer { get; set; }
+
+        public string NamePlayer { get; set; }
+
+        public Player(string namePlayer)
+        {
+            NamePlayer = namePlayer;
+        }
+    }
+}
